ffad79ad88c917878c965b34a8e5f7a20fbcf3e1
AnishPowar01 <powaranish74@gmail.com>
2023-02-02 15:34:00 +0530

    Added Header-Component

---
+++ components/Header.tsx
@@ -0,0 +1,63 @@
+// import React from 'react'
+
+import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+function Header() {
+    // state management for navbar scrool effect
+
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(()=>{
+        const handleScroll = () =>{
+            if(window.scrollY>0)
+            {
+                setIsScrolled(true);
+            }
+            else
+            {
+                setIsScrolled(false);
+            }
+        }
+               window.addEventListener("scroll",handleScroll)
+
+               return () =>{
+                window.removeEventListener("scroll",handleScroll)
+               }
+    },[])
+
+    // 
+  return (
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+        {/* leftside */}
+        <div className="flex items-center space-x-2 md:space-x-10">
+            <img src="https://rb.gy/ulxxee" width={100} height={100} alt="" className="cursor-pointer object-contain" />
+
+            <ul className="hidden space-x-4 md:flex">
+                <li className="headerLink">Home</li>
+                <li className="headerLink">Tv Shows</li>
+                <li className="headerLink">Movies</li>
+                <li className="headerLink">New & Popular</li>
+                <li className="headerLink">My List</li>
+            </ul>
+        </div>
+        {/* rightSide */}
+        
+        <div className="flex items-center space-x-4 text-sm font-light">
+        <MagnifyingGlassIcon className="hidden h-6 w-6 sm:inline "/>
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6"/>
+        <Link href="/account">
+        <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+        </div>
+    </header>
+  )
+}
+
+export default Header
